fe140c9e6a44b3741670fbcb9fce280777c0fb52
onursteknik <onur.saral@gazi.edu.tr>
2017-04-30 14:42:56 +0300

    MoleShooter

---
+++ MooleShooter/Form1.Designer.cs
@@ -0,0 +1,165 @@
+﻿namespace MooleShooter
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblIska = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.lblSkor = new System.Windows.Forms.Label();
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.label2 = new System.Windows.Forms.Label();
+            this.lblOran = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(358, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Başlat";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.Location = new System.Drawing.Point(4, 46);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(615, 310);
+            this.panel1.TabIndex = 1;
+            this.panel1.Click += new System.EventHandler(this.panel1_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(439, 17);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(31, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "ISKA";
+            // 
+            // lblIska
+            // 
+            this.lblIska.AutoSize = true;
+            this.lblIska.Location = new System.Drawing.Point(489, 17);
+            this.lblIska.Name = "lblIska";
+            this.lblIska.Size = new System.Drawing.Size(13, 13);
+            this.lblIska.TabIndex = 2;
+            this.lblIska.Text = "0";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 17);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(37, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "SKOR";
+            // 
+            // lblSkor
+            // 
+            this.lblSkor.AutoSize = true;
+            this.lblSkor.Location = new System.Drawing.Point(62, 17);
+            this.lblSkor.Name = "lblSkor";
+            this.lblSkor.Size = new System.Drawing.Size(13, 13);
+            this.lblSkor.TabIndex = 2;
+            this.lblSkor.Text = "0";
+            // 
+            // timer2
+            // 
+            this.timer2.Interval = 500;
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(161, 17);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(38, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "ORAN";
+            // 
+            // lblOran
+            // 
+            this.lblOran.AutoSize = true;
+            this.lblOran.Location = new System.Drawing.Point(211, 17);
+            this.lblOran.Name = "lblOran";
+            this.lblOran.Size = new System.Drawing.Size(13, 13);
+            this.lblOran.TabIndex = 2;
+            this.lblOran.Text = "0";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(623, 396);
+            this.Controls.Add(this.lblOran);
+            this.Controls.Add(this.lblSkor);
+            this.Controls.Add(this.lblIska);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Moole";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblIska;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label lblSkor;
+        private System.Windows.Forms.Timer timer2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lblOran;
+    }
+}
+
---
+++ MooleShooter/Form1.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MooleShooter
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Doldur();
+        }
+        Random rnd = new Random();
+
+        void Doldur()
+        {
+            timer1.Start();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            timer1.Interval = 1000;
+            panel1.Controls.Clear();
+            Button btn = new Button();
+            btn.Size = new Size(50,50);
+            btn.BackgroundImage = Properties.Resources.Yellow_Bird;
+            btn.BackgroundImageLayout = ImageLayout.Zoom;
+            btn.BackColor = Color.Transparent;
+            btn.FlatStyle = FlatStyle.Flat;
+            btn.FlatAppearance.BorderSize = 0;
+            btn.FlatAppearance.MouseOverBackColor = Color.White;
+            btn.FlatAppearance.MouseDownBackColor = Color.White;
+            btn.Location = new Point(rnd.Next(0,565),rnd.Next(0,260));
+            btn.Click += Btn_Click;
+            panel1.Controls.Add(btn);
+        }
+
+        private void Btn_Click(object sender, EventArgs e)
+        {
+            timer1.Stop();
+            timer2.Start();
+            skor += 1;
+            lblSkor.Text = skor.ToString();
+            oran = (skor*100)/(skor + int.Parse(lblIska.Text));
+            lblOran.Text = "%" + oran;
+            Button btn = (Button)sender;
+            PictureBox pb = new PictureBox();
+            pb.Image = Properties.Resources.blood_PNG6104;
+            pb.SizeMode = PictureBoxSizeMode.StretchImage;
+            pb.Size = new Size(80, 80);
+            Point pbp = btn.Location;      // Aşağıdaki 4 satırda kan resminin yerini belirliyoruz. Dengeli olması açısından kuş resminin konumundan biraz yukarı sola doğru alıyoruz ki ortalı düzgün gözüksün.
+            pbp.X -= 15;
+            pbp.Y -= 15;
+            pb.Location = new Point(pbp.X, pbp.Y);   
+            panel1.Controls.Add(pb);
+            lblSkor.Tag = pb;  //Tag özelliğini gizli bir cep olarak düşünebilirsiniz. Her türlü (1) adet objeyi içine alabilir.Ben içine picturebox ı attım.Kuşu vurabilirsek timer2 çalışacak. Timer2 kuş resmini kaldıracak ve timer1 i çalıştıracak. Timer1 in hemen devreye girmesi için aşağıda timer1 in interval ini 10 yapacağız. Kuşu vurduğumuz an kan gözükecek ve timer2 nin interval i olan 500ms sonra form hemen eski haline geri gelecek. Yani biz kuşu vurduysak 510 ms sonra form eski haline dönüyor.
+            panel1.Controls.Remove(btn);
+            
+        }
+
+        //Cursor u değiştirmek için gerekli - windows api kullanıyoruz 
+        //Detaylı öğreneceğim diyorsanız Türkçe kaynak adresi burada :)
+        // http://www.csharpnedir.com/articles/read/?id=83
+        //Daha basitçe anlatım ise burada
+        // https://www.teknologweb.com/c-ile-windows-api-kullanimi
+        [System.Runtime.InteropServices.DllImport("user32.dll")]
+        public static extern IntPtr LoadCursorFromFile(string fileName);
+
+        
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            IntPtr handle = LoadCursorFromFile("sniper.cur"); //Buradaki 3 satırda formun cursor'unu sniper yaptık.
+            Cursor cc = new Cursor(handle);
+            this.Cursor = cc;
+        }
+        int skor = 0;
+        int iska = 0;
+        double oran = 0;
+        private void panel1_Click(object sender, EventArgs e)
+        {
+            //Kuşa değil de panele tıklarsa ıskalamış sayılacak.Iska sayısını artırıp gösterecek gerekli kodları yazdık.
+            iska += 1;
+            lblIska.Text = iska.ToString();
+            int skr = int.Parse(lblSkor.Text);
+            oran = (skr * 100) / (skr + iska);
+            lblOran.Text = "%" + oran;
+        }
+
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            PictureBox pb = (PictureBox)lblSkor.Tag;
+            panel1.Controls.Remove(pb);
+            timer2.Stop();
+            timer1.Start();
+            timer1.Interval = 10;
+        }
+    }
+}
